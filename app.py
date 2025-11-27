679b5357784a8c4776d6b1b1feeda7bfddba35af
shreya <shreyaraikedenji@gmail.com>
2025-11-27 09:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Homepage
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-# About Page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-# Founder Page
+
 @app.route("/founder")
 def founder():
     return render_template("founder.html")
 
-# Training Programs
 @app.route("/programs")
 def programs():
     return render_template("programs.html")
 
-# Kids Performance Tracker
+
 @app.route("/tracker")
 def kids_performance():
     return render_template("tracker.html")
 
-# Gallery Page
+
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
@@ -42,3 +40,4 @@ def join():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
